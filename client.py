971ed9b63400388385e135670f0d7042debfd24a
LucasHolandaBarros <142419627+LucasHolandaBarros@users.noreply.github.com>
2025-04-03 20:01:37 -0300

    Atualizando o modo de operação e limitador de caracteres

--- client.py
+++ client.py
@@ -9,11 +9,18 @@ client.connect((HOST, PORT))
 print("Conexão efetuada com o servidor!")
 print(f"\nConexão: Host {HOST} Porta {PORT}\n")
 
+print("Digite a quantidade de caracteres a ser digitado: ")
+qntd = int(input())
+
 while True:
     msg = input("Digite: ")
+    if len(msg) <= qntd:
+        client.send(msg.encode("utf-8"))
+    else:
+        msg = msg[:qntd]
+        client.send(msg.encode("utf-8"))
     if not msg:
         break
-    client.send(msg.encode())
 
 print("\nConexão encerrada com o servidor\n")
 
--- server.py
+++ server.py
@@ -3,6 +3,20 @@ from socket import *
 HOST = '127.0.0.1'
 PORT = 55551
 
+print("Escolha o modo de operação: ")
+print("[1] - Go-Back-N")
+print("[2] - Repetição Seletiva")
+
+modo = input("Digite o número da operação: ")
+
+if modo == "1":
+    print("\nModo escolhido: Go-Back-N\n")
+elif modo == "2":
+    print("\nModo escolhido: Repetição Seletiva\n")
+else:
+    print("\nModo inválido! Encerrando o servidor!\n")
+    exit()
+
 print(f'Servidor iniciado em {HOST} porta: {PORT}')
 print("\nAguardando conexão...")
 
@@ -11,7 +25,7 @@ server.bind((HOST, PORT))
 server.listen(5)
 
 con, adr = server.accept()
-print(f'Cliente conectado: {adr}')
+print(f'Cliente conectado: {adr}\n')
 
 mensagem_full = ""
 
